c53c86fb63e7ac294a3cc0e7ff254d6faac4b1c8
황유정 <@192.168.14.43>
2021-05-06 23:06:08 +0900

    Committed

--- app/src/main/java/edu/handong/csee/java/hw2/IntegratedConverter.java
+++ app/src/main/java/edu/handong/csee/java/hw2/IntegratedConverter.java
@@ -8,55 +8,5 @@ import edu.handong.csee.java.hw2.converters.*; // You will learn the import stat
  * This is a class IntegratedConverter, which contains main method and run method that practically runs command.  
  */
 public class IntegratedConverter {
-
-    private double fromValue;
-    /**
-     * This is a main method of this program.
-     */
-    public static void main(String[] args) {
-
-        IntegratedConverter myConverter = new IntegratedConverter();
-
-        myConverter.run(args);
-
-    }
-
-    private void run(String[] args) {
-
-        fromValue = Double.parseDouble(args[0]);
-        String originalMeasure = Util.getUppercaseString(args[1]);
-        String targetMeasure = Util.getUppercaseString(args[2]);
-
-        if(!targetMeasure.equals("ALL")) {
-
-            String converterName = "edu.handong.csee.java.hw2.converters." + originalMeasure + "To" + targetMeasure + "Converter";
-
-            // You will learn about the try-catch block and Exception in L19
-            try {
-
-                // Advanced Java feature: Reflection (it lets us use classes with their String class names.)
-                // (JC does not teach this topic as it is advanced one but you can study by yourself!!)
-                // https://www.oracle.com/technical-resources/articles/java/javareflection.html
-                Class<?> converterClass = (Class<?>) Class.forName(converterName);
-                Constructor<?> constructor = (Constructor<?>) converterClass.getConstructor();
-                Convertible myConverter = (Convertible) constructor.newInstance();
-                myConverter.setFromValue(fromValue);
-                myConverter.convert();
-                System.out.println(fromValue +" " + originalMeasure + " is " + myConverter.getConvertedValue() + " " + targetMeasure + "!");
-
-            } catch(ClassNotFoundException | NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-                System.out.println("The converter (" + converterName + ") for " + originalMeasure + " to " + targetMeasure + " does not exist!!");
-            }
-
-        } else {
-
-            AllConverter myAllConverter = new AllConverter();
-            // When your method returns `this' in methods of AllConverter, you can call them in this way (method chaining).
-            // Think/understand how and why this work. Study and search for Method chaining
-            // https://stackoverflow.com/questions/21180269/how-to-achieve-method-chaining-in-java
-            // https://www.geeksforgeeks.org/method-chaining-in-java-with-examples/#:~:text=Method%20Chaining%20is%20the%20practice,with%20a%20(dot.).
-            myAllConverter.setFromValue(fromValue).setOriginalMeasure(originalMeasure).convertAndPrintOut();
-        }
-    }
 }
 
